f2b69efc734af85b76cfe1caf476784f6ca2b1d2
Leozer HD <leonardminoto@gmail.com>
2022-12-28 14:04:28 +0700

    test

---
+++ app/src/main/java/acid/istts/parkremobile/activities/shared/admin/main_admin.kt
@@ -0,0 +1,12 @@
+package acid.istts.parkremobile.activities.shared.admin
+
+import acid.istts.parkremobile.R
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class main_admin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_admin)
+    }
+}
