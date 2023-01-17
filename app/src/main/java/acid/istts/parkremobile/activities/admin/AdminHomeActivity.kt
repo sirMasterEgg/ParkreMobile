4142fce6c728898406adff12e2435a0074806b9f
lele416 <kriptoleonardoc@gmail.com>
2023-01-17 16:21:49 +0800

    volley admin logout

--- app/src/main/java/acid/istts/parkremobile/activities/admin/AdminHomeActivity.kt
+++ app/src/main/java/acid/istts/parkremobile/activities/admin/AdminHomeActivity.kt
@@ -1,10 +1,13 @@
 package acid.istts.parkremobile.activities.admin
 
 import acid.istts.parkremobile.R
+import acid.istts.parkremobile.activities.shared.LoginActivity
 import acid.istts.parkremobile.adapters.admin.AnnoucementAdapter
 import acid.istts.parkremobile.adapters.staff.AnnouncementAdapter
 import acid.istts.parkremobile.fragments.admin.*
 import acid.istts.parkremobile.models.Announcement
+import acid.istts.parkremobile.services.AppDatabase
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
@@ -12,7 +15,15 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.google.android.material.navigation.NavigationView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class AdminHomeActivity : AppCompatActivity() {
 
@@ -34,12 +45,20 @@ class AdminHomeActivity : AppCompatActivity() {
     lateinit var admin_detail_mall : AdminMallDetailFragment
     lateinit var admin_detail_announcement : AdminAnnouncementDetail
 
+    val ioScope = CoroutineScope(Dispatchers.IO)
+    val db = AppDatabase.build(this)
+
     private lateinit var annAdapter: AnnoucementAdapter
     var annListadmin : ArrayList<Announcement> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_home)
+
+        var token: String? = null
+        runBlocking {
+            token = db.userDAO.getToken()
+        }
 
         drawerLayout = findViewById(R.id.drawer_layout)
         navigationView = findViewById(R.id.sidebar_view)
@@ -86,6 +105,29 @@ class AdminHomeActivity : AppCompatActivity() {
                 R.id.sidebar_master_announcement->{
                     gantihalamanannounce()
                     drawerLayout.closeDrawer(GravityCompat.START)
+                }
+                R.id.sidebar_logout->{
+                    ioScope.launch {
+                        db.userDAO.clear()
+                    }
+                    val req = object : StringRequest(
+                        Method.POST, "https://parkre.loca.lt/api/logout", Response.Listener {},
+                        Response.ErrorListener {
+                        println("====================================")
+                        println(String(it.networkResponse.data, Charsets.UTF_8))
+                    }){
+                        override fun getHeaders(): MutableMap<String, String> {
+                            val headers = HashMap<String, String>()
+                            headers["Authorization"] = token!!
+                            headers["Accept"] = "application/json"
+                            return headers
+                        }
+                    }
+                    val queue : RequestQueue = Volley.newRequestQueue(this)
+                    queue.add(req)
+
+                    val intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
                 }
             }
             drawerLayout.closeDrawer(GravityCompat.START)
